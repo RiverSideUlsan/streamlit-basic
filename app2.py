1025a6e1d44aeafdbd3a4dabcbd3e5115dfe70d9
LEE SEUNGRYEOL <riversideulsan@gmail.com>
2026-09-18 11:08:19 +0900

    secret key 사용방식 원래대로 변경(.toml)

--- app2.py
+++ app2.py
@@ -3,7 +3,9 @@ from pathlib import Path
 import streamlit as st
 
 
-APP_PAGE = Path(__file__).with_name("stream_pages") / "main.py"
+PROJECT_ROOT = Path(__file__).parent
+APP_PAGE = PROJECT_ROOT / "stream_pages" / "main.py"
+
 app = st.App(APP_PAGE)
 
 if __name__ == "__main__":
